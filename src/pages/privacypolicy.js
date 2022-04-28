23dbaa68dbb32931cf6abb73bd9669ed56240ce0
Subaan Qasim <96848399+subaanqasim@users.noreply.github.com>
2022-04-28 16:36:00 +0100

    Updated privacy policy

--- src/pages/privacypolicy.js
+++ src/pages/privacypolicy.js
@@ -6,7 +6,7 @@ import remarkGfm from "remark-gfm"
 
 const markdown = `
 # PRIVACY NOTICE
-**Last updated April 19, 2022**
+**Last updated April 28, 2022**
 
 This privacy notice for The Muslim's Dashboard (**"Company," "we," "us,"** or **"our"**), describes how and why we might collect, store, use and/or share (**"process"**) your information when you use our services(**"Services"**), such as when you:
 - Download and use our web application (The Muslim's Dashboard), or any other application of ours that links to this privacy notice
@@ -17,28 +17,147 @@ This privacy notice for The Muslim's Dashboard (**"Company," "we," "us,"** or **
 ## SUMMARY OF KEY POINTS
 This summary provides key points from our privacy notice, but you can find out more details about any of these topics by clicking the link following each key point or by using our table of contents below to find the section you are looking for.
 
-**What personal information do we process?** When you visit, use, or navigate our Services, we may process personal information depending on how you interact with The Muslim's Dashboard and the Services, the choices you make, and the products and features you use. *Click here to learn more.*
+**What personal information do we process?** When you visit, use, or navigate our Services, we may process personal information depending on how you interact with The Muslim's Dashboard and the Services, the choices you make, and the products and features you use.
 
 **Do we process any sensitive personal information?** We do not process sensitive personal information.
 
 **Do you receive any information from third parties?** We do not receive any information from third parties.
 
-**How do you process my information?** We process your information to provide, improve and administer our Services, communicate with you, for security and fraud prevention, and to comply with law. We may also process your information for other purposes with your consent. We process your information only when we have a valid legal reason to do so. *Click here to learn more*
+**How do you process my information?** We process your information to provide, improve and administer our Services, communicate with you, for security and fraud prevention, and to comply with law. We may also process your information for other purposes with your consent. We process your information only when we have a valid legal reason to do so.
 
-**In what situations and with which parties do we share personal information?** We may share information in specific situation and with specific third parties. *Click here to learn more.*
+**In what situations and with which parties do we share personal information?** We may share information in specific situation and with specific third parties.
 
-**How do we keep your information safe?** We have organisational and technical processes and procedures in place to protect your personal information. However, no electronic transmission over the internet or information storage technology can be guaranteed to be 100% secure, so we cannot promise or guarantee that hackers, cybercriminals, or other unauthorised third parties will not be able to defeat our security and improperly collect, access, steal or modify your information. *Click here to learn more.*
+**How do we keep your information safe?** We have organisational and technical processes and procedures in place to protect your personal information. However, no electronic transmission over the internet or information storage technology can be guaranteed to be 100% secure, so we cannot promise or guarantee that hackers, cybercriminals, or other unauthorised third parties will not be able to defeat our security and improperly collect, access, steal or modify your information.
 
-**What are your rights?** Depending on where you are located geographically, the applicable privacy law may mena you have certain rights regarding your personal information. *Click here to learn more.*
+**What are your rights?** Depending on where you are located geographically, the applicable privacy law may mean you have certain rights regarding your personal information.
 
 **How do I exercise my rights?** The easiest way to exercise your rights is by filling out our data subject request form available [here](https://app.termly.io/notify/6fd3583f-a3f4-434d-8331-4c31412c3d4b), or by contacting us. We will consider and act upon any request in according with applicable data protection laws.
+
+
+## TABLE OF CONTENTS
+1. WHAT INFORMATION DO WE COLLECT?
+2. HOW DO WE PROCESS YOUR INFORMATION?
+3. WHAT LEGAL BASES DO WE RELY ON TO PROCESS YOUR PERSONAL INFORMATION?
+4. WHEN AND WITH WHOM DO WE SHARE YOUR PERSONAL INFORMATION?
+5. HOW LONG DO WE KEEP YOUR INFORMATION?
+6. HOW DO WE KEEP YOUR INFORMATION SAFE?
+7. DO WE COLLECT INFORMATION FROM MINORS?
+8. WHAT ARE YOUR PRIVACY RIGHTS?
+9. DO WE MAKE UPDATES TO THIS NOTICE?
+10. HOW CAN YOU CONTACT US ABOUT THIS NOTICE?
+11. HOW CAN YOU REVIEW, UPDATE, OR DELETE THE DATA WE COLLECT FROM YOU?
+
+## 1. WHAT INFORMATION DO WE COLLECT?
+### Personal information you disclose to us
+***In short:*** *We collect personal information that you provide to us.
+We collect personal information that you voluntarily provide to us when you express an interest in obtaining information about us or our products and Services, or otherwise when you contact us.*
+
+**Sensitive Information.** We do not process sensitive information.
+
+**Payment Data.** We may collect data necessary to process your payment if you make purchases, such as your payment instrument number (such as credit card number), and the security code associated with your payment instrument. All payment data is stored by Stripe. You may find their privacy notice link(s) here: https://stripe.com/en-gb/privacy.
+
+**Application Data.** If you use our application(s), we also may collect the following information you choose to provide us with access or permission:
+- *Geolocation Information.* We may request access of location-based information from your device while you are using our application(s) to provide certain location-based services (prayer times and weather). If you wish to change our access or permissions, you may do so in your device's settings. Please note that this information is briefly sent to the servers to calculate weather data and is not stored outside of your personal device(s); prayer times are calculated locally.
+
+This information is primarily needed to maintain the security and operation of our applications(s), for troubleshooting, and for our internal analytics and reporting purposes.
+
+All personal information that you provide to us must be true, complete, and accurate, and you must notify us of any changes to such personal information.
+
+## Information automatically collected
+***In short:*** *Some information—such as your Internet Protocol (IP) address and/or browser and device characteristics—is collected automatically when you visit our Services.*
+
+We automatically collect certain information when you visit, use, or navigate, the Services. This information does not reveal your specific identity (like your name or contact information) but may include device and usage information, such as your IP address, browser and device characteristics, operating system, language preferences, referring URLs, device name, country, location, information about how and when you use our services, and other technical information. This information is primarily needed to maintain the security and operations of our Services, and for our internal analytics and reporting purposes.
+
+The information we collect includes:
+- *Log and Usage Data.* Log and usage data is service-related, diagnostic, usage, and performance information with our servers automatically collect when you access or use our Services and which we record in log files. Depending on how you interact with us, this log data may include your IP address, device information, browser type, and information about your activity in the Services (such as the date/time stamps associated with your usage, pages and files viewed, searches, and other actions you take such as which features you use), device event information (such as system activity, error reports (sometimes called “crash dumps”), and hardware settings).
+
+## 2. HOW DO WE PROCESS YOUR INFORMATION?
+***In short:*** *We process your information to provide, improve, and administer our Services, communicate with you, for security and fraud prevention, and to comply with law. We may also process your information for other purposes with your consent.*
+
+**We process your personal information for a variety of reasons, depending on how you interact with our Services, including:**
+- **To save or protect an individual’s vital interest.** We may process your information when necessary to save or protect an individual’s vital interest, such as to prevent harm.
+
+## 3. WHAT LEGAL BASES DO WE RELY ON TO PROCESS YOUR INFORMATION?
+***In short:***  *We only process your personal information when we believe it is necessary and we have a valid legal reason (i.e., legal basis) to do so under applicable law, like with your consent, to comply with laws, to provide you with serves to enter into or fulfil our contractual obligations, to protect your rights, or to fulfil our legitimate business interests.*
+
+***If you are located in the EU or UK, this section applies to you.***
+The General Data Protection Regulation (GDPR) and UK GDPR require us to explain the valid legal bases we rely on in order to process your personal information. As such, we may rely on the following legal bases to process your personal information:
+- **Consent.** We may process your information if you have given us permission (i.e., consent) to use your personal information for a specific purpose. You can withdraw your consent at any time.
+- **Legal Obligations.** We may process your information where we believe it is necessary for compliance with our legal obligations, such as to cooperate with a law enforcement body or regulatory agency, exercise or defend our legal rights, or disclose your information as evidence in litigation in which we are involved.
+- **Vital Interests.** We may process your information where we believe it is necessary to protect your vital interests or the vital interests of a third party, such as situations involving potential threats to the safety of any person.
+
+***If you are located in Canada, this section applies to you.***
+We may process your information if you have given us specific permission (i.e., express consent) to use your personal information for a specific purpose, or in situations where your permission can be inferred (i.e., implied consent). You can withdraw your consent at any time.
+
+In some exceptional cases, we may be legally permitted under applicable law to process your information without your consent, including, for example:
+- If collection is clearly in the interests of an individual and consent cannot be obtained in a timely way
+- For investigations and fraud detection and prevention
+- For business transactions provided certain conditions are met
+- If it is contained in a witness statement and the collection is necessary to assess, process, or settle and insurance claim
+- For identifying injured, ill, or deceased persons and communicating with next of kin
+- If we have reasonable grounds to believe an individual has been, is, or may be victim of financial abuse
+- If it is reasonable to expect collection and use with consent would compromise the availability or the accuracy of the information and the collection is reasonable for purposes related to investigation a breach of an agreement or a contravention of the laws of Canada or a province
+- If disclosure is required to comply with a subpoena, warrant, court order, or rules of the court relation to the production of records
+- If it was produced by an individual in the course of their employment, business, or professional and the collection is consistent with the purposes for which the information was produced
+- If the collection is solely for journalistic, artistic, or literary purposes
+- If the information is publicly available and is specified by the regulations
+
+## 4. WHEN AND WITH WHOM DO WE SHARE YOUR PERSONAL INFORMATION?
+***In short:*** *We may share information in specific situations described in this section and/or with the following third parties.*
+
+We may need to share your personal information in the following situations:
+- **Business Transfers**. We may share or transfer your information in connection with, or during negotiations of, any merger, sale of company assets, financing, or acquisition of all or a portion of our business to another company.
+
+## 5. HOW LONG DO WE KEEP YOUR INFORMATION?
+***In short:*** *We keep your information for as long as necessary to fulfil the purposes outlined in this privacy notice unless otherwise required by law.*
+
+We will only keep your personal information for as long as it is necessary for the purposes set out in this privacy notice, unless a longer retention period is required or permitted by law (such as tax, accounting, or other legal requirements). No purpose in this notice will require us keeping your personal information for 90 days.
+
+When we have no ongoing legitimate business need to process your personal information, we will either delete or anonymise such information, or, if this is not possible (for example, because your personal information has been stored in backup archives), then we will securely store your personal information and isolate it from any further processing until deletion is possible.
+
+## 6. HOW DO WE KEEP YOUR INFORMATION SAFE?
+***In short:*** *We aim to protect your personal information through a system of organisational and technical security measures.*
+
+We have implemented appropriate and reasonable technical and organisational security measures designed to protect the security of any personal information we process. However, despite our safeguards and efforts to secure your information, no electronic transmission of the Internet or information storage technology can be guaranteed to be 100% secure, so we cannot promise or guarantee that hackers, cybercriminals, or other unauthorised third parties will not be able to defeat our security and improperly collect, access, steal, or modify your information. Although we will do our best to protect your personal information, transmission of personal information to and from our Services is at your own risk. You should only access the Services within a secure environment.
+
+## 7. DO WE COLLECT INFORMATION FROM MINORS?
+**In short:** *We do not knowingly collect data from or market to children under 18 years of age.*
+
+We do not knowingly solicit data from or market to children under 18 years of age. By using the Services, you represent that you are at least 18 or that you are the parent or guardian of such a minor and consent tto such minor dependent’s use of the Services. If we learn that personal information form users less than 18 years of age has been collected, we will take reasonable measures to promptly delete such data from our records. If you become aware of any data we may have collected from children under age 18, please contact us at subaan.qasim@gmail.com.
+
+## 8. WHAT ARE YOUR PRIVACY RIGHTS?
+**In short:** *In some regions, such as the European Economic Area (EEA), United Kingdom (UK), Canada, you have rights that allow you greater access to and control over your personal information. You ay review, change, or terminate your account at any time.*
+
+In some regions (like the EEA, UK, and Canada), you have certain rights under applicable data protection laws. These may include the right (i) to request access and obtain a copy of your personal information, (ii) to request rectification or erasure; (iii) to restrict the procession of your personal information; and (iv) if applicable, to data portability. In certain circumstances, you may also have the right to object to the processing of your personal information. You can make such a request by contacting us using the contact details provided in the section “HOW CAN YOU CONTACT US ABOUT THIS NOTICE?” below.
+
+We will consider and act upon any request in accordance with applicable data protection laws.
+
+If you are located in the EEA or UK and you believe we are unlawfully processing your personal information, you also have the right to complain to your local data protection supervisory authority. You can find their contact details here: https://ec.europa.eu/justice/data-protection/bodies/authorities/index_en.html.
+
+If you are located in Switzerland, the contact details for the data protection authorities are available here: https://www.edoeb.admin.ch/edoeb/en/home.html
+
+**Withdrawing your consent:** If we are relying on your consent to process your personal information, which may be express and/or implied consent depending on the applicable law, you have the right to withdraw your consent at any time. You can withdraw your consent at any time by contacting us by using the contact details provided in the section “HOW CAN YOU CONTACT US ABOUT THIS NOTICE?” below.
+
+However, please note that this will not affect the lawfulness of the processing before its withdrawal, nor when applicable law allows, will it affect the processing of your personal information conducted in reliance on lawful processing grounds other than consent.
+
+If you have questions or comments about your privacy rights, you may email us at subaan.qasim@gmail.com.
+
+## 9. DO WE MAKE UPDATES TO THIS NOTICE?
+In short: Yes, we will update this notice as necessary to stay compliant with relevant laws.
+
+We may update this privacy notice from time to time. The updated version will be indicated by an updated “Revised” date and the updated version will be effective as soon as it is accessible. If we make material changes to this privacy notice, we may notify you either by prominently posting a notice of such changes or by directly sending you a notification. We encourage you to review this privacy notice frequently to be informed of how we are protecting your information.
+
+## 10. HOW CAN YOU CONTACT US ABOUT THIS NOTICE?
+If you have questions or comments about this notice, you may email us at subaan.qasim@gmail.com.
+
+## 11. HOW CAN YOU REVIEW, UPDATE, OR DELETE THE DATA WE COLLECT FROM YOU?
+Based on the applicable laws of your country, you have the right to request access to the personal information we collect from your, change that information, or delete it in some circumstances. To request to review, update, or delete your personal information, please submit a request form by clicking [here](https://app.termly.io/notify/6fd3583f-a3f4-434d-8331-4c31412c3d4b).
 `
 
 const privacypolicy = () => {
   return (
     <Layout>
       <Container>
-        <div>privacypolicy</div>
         <ReactMarkdown children={markdown} remarkPlugins={[remarkGfm]} />
       </Container>
     </Layout>
